e402c93ad75077bb568570b3582dd2f12882d436
czy0729 <402731062@qq.com>
2020-05-24 22:21:06 +0800

    update 2020/05/24

--- spider.js
+++ spider.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2020-01-14 18:51:27
  * @Last Modified by: czy0729
- * @Last Modified time: 2020-05-17 20:33:47
+ * @Last Modified time: 2020-05-24 22:13:51
  */
 const axios = require('axios')
 const fs = require('fs')
@@ -58,8 +58,8 @@ function fetchSubject(id) {
   })
 }
 
-const start = 356400
-const end = 356800
+const start = 356500
+const end = 357000
 
 // const start = 325000
 // const end = 330000
