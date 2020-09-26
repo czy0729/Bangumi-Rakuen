402819a0896e40a6ce22ecebddbf879f217dac1f
czy0729 <402731062@qq.com>
2020-09-26 21:07:26 +0800

    update 2020/09/26

--- spider.js
+++ spider.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2020-01-14 18:51:27
  * @Last Modified by: czy0729
- * @Last Modified time: 2020-09-13 18:02:29
+ * @Last Modified time: 2020-09-26 21:05:21
  */
 const axios = require('axios')
 const fs = require('fs')
@@ -58,7 +58,7 @@ function fetchSubject(id) {
   })
 }
 
-const start = 358800
+const start = 358950
 const end = 359200
 
 // const start = 325000
