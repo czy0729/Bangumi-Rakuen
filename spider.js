b20498cfb32a3857c93c4d6bbdc92a72c686bdc7
czy0729 <402731062@qq.com>
2021-02-17 19:22:17 +0800

    update 2021/02/17

--- spider.js
+++ spider.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2020-01-14 18:51:27
  * @Last Modified by: czy0729
- * @Last Modified time: 2021-01-28 20:19:26
+ * @Last Modified time: 2021-02-17 19:21:01
  */
 const axios = require('axios')
 const fs = require('fs')
@@ -11,7 +11,7 @@ const cheerio = require('./utils/cheerio')
 const utils = require('./utils/utils')
 
 const rewrite = true
-const start = 360830
+const start = 361300
 const end = 363000
 
 function fetchSubject(id) {
